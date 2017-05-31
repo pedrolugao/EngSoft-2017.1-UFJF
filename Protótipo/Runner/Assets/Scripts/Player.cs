5d15c9c23cc5266feb3118ec12818fc7c51342c2
Pedro Lugao <pedrogasparetto@ymail.com>
2017-05-31 13:54:08 -0300

    Correção salto
    Impossibilitei o pulo enquanto o jogador está no ar

--- Protótipo/Runner/Assets/Scripts/Player.cs
+++ Protótipo/Runner/Assets/Scripts/Player.cs
@@ -1,19 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 public class Player : MonoBehaviour {
 
     public Rigidbody rb;
     public int totalMoney;
     private int totalDistance = 0;
+    private bool noAr;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        noAr = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("space"))
         {
-            rb.AddForce(new Vector3(0f, 400f, 0f));
+            if (noAr == false)
+            {
+                noAr = true;
+                rb.AddForce(new Vector3(0f, 400f, 0f));
+            }
         }
 
         if (Input.GetKeyDown("escape"))
@@ -23,8 +31,22 @@ public class Player : MonoBehaviour {
 
         for (int i = 0; i < Input.touchCount; i++)
         {
-            if(Input.GetTouch(i).phase == TouchPhase.Began)
-                rb.AddForce(new Vector3(0f, 400f, 0f));
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+                if (noAr == false)
+                {
+                    noAr = true;
+                    rb.AddForce(new Vector3(0f, 400f, 0f));
+                }
+        }
+        print(rb.position);
+        if(rb.position.y <= 0.63)
+        {
+
+            noAr = false;
+        }
+        else
+        {
+            noAr = true;
         }
 
         this.totalDistance += 1;
@@ -40,6 +62,7 @@ public class Player : MonoBehaviour {
         if (other.gameObject.CompareTag("coin"))
         {
             this.totalMoney += 1;
+            updateText();
             other.gameObject.SetActive(false);
         }
 
@@ -49,5 +72,10 @@ public class Player : MonoBehaviour {
             Application.LoadLevel(0);
             other.gameObject.GetComponent<Rigidbody>().AddForce(200f, 0f, 0f);
         }
+    }
+
+    private void updateText()
+    {
+        
     }
 }
